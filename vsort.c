9692fa66686517f83a260b5919823f54c2b538db
Klaas-Jan Stol <kjstol@gmail.com>
2020-07-27 23:16:01 +0100

    Remove archaic parameter lists

--- vsort.c
+++ vsort.c
@@ -1,3 +1,35 @@
+/*
+
+This software was developed by employees of the National Institute 
+of Standards and Technology (NIST), an agency of the Federal 
+Government and is being made available as a public service. Pursuant 
+to title 17 United States Code Section 105, works of NIST employees 
+are not subject to copyright protection in the United States.  This 
+software may be subject to foreign copyright.  Permission in the 
+United States and in foreign countries, to the extent that NIST may 
+hold copyright, to use, copy, modify, create derivative works, and 
+distribute this software and its documentation without fee is hereby 
+granted on a non-exclusive basis, provided that this notice and 
+disclaimer of warranty appears in all copies. 
+
+THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, 
+EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED 
+TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, 
+ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE, AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE 
+DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE 
+SOFTWARE WILL BE ERROR FREE.  IN NO EVENT SHALL NIST BE LIABLE FOR 
+ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL 
+OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN ANY 
+WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON WARRANTY, 
+CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS SUSTAINED BY 
+PERSONS OR PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS 
+SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF, OR USE OF, THE 
+SOFTWARE OR SERVICES PROVIDED HEREUNDER.
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,20 +37,22 @@ typedef struct ll_struct ll_node, *ll;
 
 struct ll_struct {
     int value;
-    ll next;
+    ll  next;
 };
 
-typedef struct {
+typedef struct 
+{
     int is_branch;
     int is_join;
     int visit;
-    ll branches_to, joins_from;
+    ll  branches_to, joins_from;
 } node, *node_ptr;
 
 node nodes[10000];
-int n_nodes = 0;
+int  n_nodes = 0;
 
-void output(int k) {
+void output(int k) 
+{
     ll go_list;
 
     if (nodes[k].visit)
@@ -57,7 +91,8 @@ void output1(int k) {
     }
 }
 
-void go_deep(int k) {
+void go_deep(int k) 
+{
     int at;
     at = k;
     while (at) {
@@ -68,7 +103,7 @@ void go_deep(int k) {
 int main(int argc, char **argv) {
     int i;
     int from, to;
-    ll new_list, sort_at;
+    ll  new_list, sort_at;
     int hold;
     int min, max;
 
@@ -165,3 +200,4 @@ int main(int argc, char **argv) {
     */
     return 0;
 }
+
